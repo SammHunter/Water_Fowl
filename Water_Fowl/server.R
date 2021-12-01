1258201f41328929321e8f5be0b78024a35c24e6
SammHunter <shunter2@ncsu.edu>
2021-12-01 17:28:48 -0600

    I'm so happy!

--- Water_Fowl/server.R
+++ Water_Fowl/server.R
@@ -1,15 +1,121 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
+
+
+
+library(shiny)
+library(tidyverse)
+library(lubridate)
+library(caret)
+library(DT)
+library(doParallel)
+
+
+
+fowls$STRATUM = as.character(fowls$STRATUM)
+
+birds <- fowls %>% 
+  mutate( 
+    CanadianGoose = if_else(!is.na(CAGO_TIBN), CAGO_TIBN, CAGO_TIB), 
+    STRATUM = as.character(STRATUM)) %>%
+  
+  rename(Year = YEAR, State = STATE, TimeOfDay = CKTYPE, Plot = PLOT, Date = DATE,
+         Stratum = STRATUM, WetHab = WETHAB, Handfeed = HANDFEED, 
+         Mallard = MALL_TIB, 
+         AmBlackDuck = ABDU_TIB, 
+         WoodDuck = WODU_TIB) %>%
+
+  select(!CAGO_TIB, -CAGO_TIBN, -ends_with("_TIP")) %>%
+  pivot_longer(cols = 9:12, names_to = "Species", values_to = "Count") %>%
+  filter(!is.na(Count))
+
+
+# Fixing the dates to actual date instead of character strings
+birds$Date <- mdy(birds$Date)
+
+# These are just records where birds were seen, that have a date recorded, and fixing the date
+bird_count <- birds %>% filter(Count != 0)
+# Over 80% of the records are where no birds were seen so I removed those records
+bird_count <- bird_count %>% filter(-is.na(Date)|Date != "1900-01-01")
+bird_count[bird_count == "1930-04-20"] <- as.Date("2003-04-20")
 
 library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   
+  output$expGraph <- renderPlot({
+    range <- input$RangeUI
+    min_count <- range[1]
+    max_count <- range[2]
+    scatter <- ggplot(data = subset(bird_count, min_count <= Count & Count <= max_count)) +
+      geom_jitter(aes(x = Year, y = Count, shape = Species, color = State))
+    
+    scatter
+  })
+  
+  output$summary <- renderDT({
+    if(input$TableGroupUI2=="None"){
+      summary_table <- bird_count %>% group_by(.data[[input$TableGroupUI1]]) %>%
+        summarize(Mean = round(mean(Count),3), Median = round(median(Count), 3), 
+                  Range = paste0(min(Count), " - ", max(Count), IQR = IQR(Count)))
+      summary_table
+    }else{
+    summary_table <- bird_count %>% group_by(.data[[input$TableGroupUI1]], .data[[input$TableGroupUI2]]) %>%
+      summarize(Mean = round(mean(Count),3), Median = round(median(Count), 3), 
+                Range = paste0(min(Count), " - ", max(Count), IQR = IQR(Count)))
+    summary_table
+    }
+  })
+  
+  
+  output$tbCond <- renderPrint({
+    .data[[input$TableGroupUI]]
+  })
+
+  output$range <- renderPrint({
+    range <- input$RangeUI
+    range
+  })
+
+
+### MODELLING
+  
+  ## Linear Model
+  output$LMResults <- renderPrint({
+    split <- input$split/100
+    trainIndex <- createDataPartition(bird_count$Species, p = split, list=FALSE)
+    birds_train <- bird_count[trainIndex,]
+    birds_test <- bird_count[-trainIndex,]
+
+    req(input$predictors)
+    pred_data <- bird_count %>% select(Count, all_of(input$predictors))
+
+    withProgress(message = 'Linear Model', value=0, ({
+      lmFit1 <- train(Count ~ ., data =pred_data,
+                      method = "lm", trControl = trainControl(method = "cv", number = 5),
+                      na.action = na.exclude)
+    }))
+    summary(lmFit1)
+  })
+  
+  ## Random Forests
+  # output$RFResults <- renderPlot({
+  #   split <- input$split/100
+  #   trainIndex <- createDataPartition(bird_count$Species, p = split, list=FALSE)
+  #   birds_train <- bird_count[trainIndex,]
+  #   birds_test <- bird_count[-trainIndex,]
+  #   
+  #   req(input$predictors)
+  #   pred_data <- bird_count %>% select(Count, all_of(input$predictors))
+  #   
+  #   withProgress(message = 'Random Forests', value=0, ({
+  #     rf_SigPred <- train(Count ~ Year + State + Handfeed + Species,
+  #     data = birds_train, method = "rf",
+  #     trControl = trainControl(method = "cv", number = 5),
+  #     tuneGrid = data.frame(mtry = c(1, 2, 3, 4)))
+  #     })) 
+  #   rf_SigPred
+  #   plot(rf_SigPred)
+  # })
 })
+
--- Water_Fowl/ui.R
+++ Water_Fowl/ui.R
@@ -1,23 +1,26 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
+
+
+
+
+
+
+
 
 library(shiny)
 library(tidyverse)
 library(lubridate)
 library(caret)
+library(DT)
+library(doParallel)
+
 
 # R Studio Layout guide (about halfway down) explains navbarPage() >>> https://shiny.rstudio.com/articles/layout-guide.html
 
 fluidPage(
     navbarPage("Water Fowl Counts along the American North-East Coast",
     
-      tabPanel("About"), 
+      #tabPanel("About"), 
         # IN ABOUT:
         #     - Mention that you don't know if a blank count is a zero or missing so I treated it as zero to be on the 
         #       conservative side for the birds
@@ -30,53 +33,105 @@ fluidPage(
       
         # - Describe purpose of app, discuss data and source, purpose of each page, include a picture
       
-      tabPanel("Data Exploration", 
+      # tabPanel("Data Exploration", 
+      #          
+      #          column(6, # The radio button decides which variable to explore in the graph
+      #                 radioButtons("DisplayGraph", "Choose the Variable to Explore",
+      #                                  c("Sepcies" = 1, "Year" = 2, "State" = 3, "Stratum", "Time of Day" = 4, #"TimeOfDay", 
+      #                                    "Wetland Habitat" = 5)),#"WetHab")),
+      #                 #textOutput("tbCond"), 
+      #                 
+      #                 # This is only displayed for the Species selection
+      #                 # Need the slider to remove values of count in the scatter plot based on Species
+      #                 conditionalPanel(condition = "input.DisplayGraph == 1", 
+      #                                  sliderInput("RangeUI", "Limit the Values for Number of Birds Seen", 
+      #                                              min = 0, max = 400, value = c(1, 50)),
+      #                                  
+      #                                  # checkboxGroupInput("SpeciesUI", "Choose the Species",
+      #                                  #                    c("American Black Duck" = "AmBlackDuck",
+      #                                  #                      "Canadian Goose" = "CanadianGoose",
+      #                                  #                      "Mallard" = "Mallard",
+      #                                  #                      "Wood Duck" = "WoodDuck")),
+      #                                  # textOutput("tbCond"),
+      #                                  textOutput("range"),
+      #                 ),
+      #                 
+      #                 submitButton("Graph"),
+      #                 plotOutput("expGraph")),
+      #          
+      #         column(3, 
+      #                radioButtons("TableGroupUI1", "First Variable Grouping",
+      #                                   c("Species","Year", "State", "Stratum", "Time of Day" = "TimeOfDay",
+      #                                     "Wetland Habitat" = "WetHab"))),
+      #         column(3,
+      #                radioButtons("TableGroupUI2", "Second Variable Grouping",
+      #                             c("None", "Species","Year", "State", "Stratum", "Time of Day" = "TimeOfDay",
+      #                               "Wetland Habitat" = "WetHab")),
+      #                submitButton("Table"),
+      #                textOutput("tbCond")),
+      #                # textOutput("vars"),
+      #         column(6,
+      #                DTOutput("summary")), 
+      #         ),
+      
+              # these should be used if you want to show only these vars...maybe use for histogram or bar graphs
+                     # Put these in their own categories...choosing them should produce different things
+                     
+                     
+                     # # Bar plot of species counted during what times of day
+                     # selectInput("TODUI", "Time Of Day", 
+                     #             c("Mid-Day" = 1, 
+                     #               "Twilight" = 2, 
+                     #                 "Not Sampled" = 3)),
+                     #  
+                     # selectInput("StratumUI", "Stratum/Habitat", 
+                     #              c(unique(birds$Stratum))), 
+                     #  
+                     # radioButtons("WetHabUI", "Wetland Habitat", 
+                     #               c("Yes", "No")),
+                     #  
+                     # radioButtons("HandfeedUI", "Handfeeder Present?",
+                     #               c("Yes", "No")), )
+              
                
-               # Put these in their own categories...choosing them should produce different things
-               checkboxGroupInput("SpeciesUI", "Choose the Species", 
-                                  c("American Black Duck" = "AmBlackDuck", 
-                                    "Canadian Goose" = "CanadianGoose", 
-                                    "Mallard" = "Mallard", 
-                                    "Wood Duck" = "WoodDuck")),
-               
-               # Bar plot of species counted during what times of day
-               selectInput("TODUI", "Time Of Day", 
-                           c("Mid-Day" = 1, 
-                             "Twilight" = 2, 
-                             "Not Sampled" = 3)),
-               
-               selectInput("StratumUI", "Stratum/Habitat", 
-                           c(unique(birds$Stratum))), 
-               
-               radioButtons("WetHabUI", "Wetland Habitat", 
-                            c("Yes", "No")),
-               
-               radioButtons("HandfeedUI", "Handfeeder Present?",
-                            c("Yes", "No")), 
-               
-               submitButton("Quack")),
         # Numerical and graphical summaries, change type of plot and type of summary reported, change variables and filter the 
         # the rows to change the data in the plots/summaries
         
         navbarMenu("Modelling",
                    
-          tabPanel("Information"
-                   
+          tabPanel("Information on Supervised Learning Methods",
+                   h4("Multiple Linear Regression, Regression Trees, and Random Forest Model"),
+                   br(), 
+                   h4("")
                    ),
           # Explain the three modeling approaches, inc the pros and cons of each
           # NEED mathJax
           
-          tabPanel("Model Fitting"), 
-          # Split data into training and test, allowing the user to choose the proportion of data used in each
-          # Need to be able to choose the variables used (Year, State, Stratum, TimeofDay, WetHab, Handfeed)
-          # ALL MODELS NEED TO INCLUDE COUNT AND SPECIES??
-          #### Don't change until button is pressed
-          # Fit stats should be reported for each model
-          # Models sb compared on test sets
-          
-        
+          tabPanel("Model Fitting",
+                   sliderInput("split", "Choose the Percentage Of Total Observations in Training Set", 
+                               min = 50, max = 90, value = 70), 
+                   selectInput("predictors", "Predictors for Bird Count", 
+                                names(birds), multiple = TRUE, selectize = FALSE), 
+                   
+                   textOutput("Preds"), 
+                   submitButton("Model"),
+                   
+                   verbatimTextOutput("LMResults"),
+                   plotOutput("RFResults")
+                   
+                   ), 
+
         # Will want to pass an argument and result in count of birds 
-          tabPanel("Prediction")
+          tabPanel("Prediction",
+                   selectInput("predSpecies", "Choose a Species",
+                               unique(birds$Species)),
+                   numericInput("predYear", "Choose a Year between 1993 through 2015",
+                                min = 1993, max = 2015, value=2000),
+                   selectInput("predState", "Pick an North-Eastern State",
+                               unique(birds$State)),
+                   selectInput("predStrat", "Pick a Stratum",
+                                unique(birds$Stratum))
+                   )
           # Select the values of the predictors and obtain a prediction for the response
         # Have Shiny throw an error if User tries to go under 1993, 2010, 2012. or over 2015
         ), 
